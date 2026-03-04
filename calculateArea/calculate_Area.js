cec7f77d74d85e2ed6f162f1c327597d506a7589
LePrinceAlex <alexis.chr270@gmail.com>
2026-03-04 07:01:38 -0500

    first function js end

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,12 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function groceryTracker() {
+    let amount_1 = parseFloat(document.getElementById('grocery1').value);
+    let amount_2 = parseFloat(document.getElementById('grocery2').value);
+    let amount_3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = amount_1 + amount_2 + amount_3;
+    document.getElementById('resultGrocery').innerText = `The total amount is: ${total}`;
+}
